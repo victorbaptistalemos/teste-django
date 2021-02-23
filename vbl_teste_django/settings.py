9154154f694f4a01297d1a148e794d61baac029d
Victor Baptista Lemos <victorbaptistalemos@icloud.com>
2021-02-23 04:32:42 +0000

    Configuração do python-decouple no Travis-CI

--- vbl_teste_django/settings.py
+++ vbl_teste_django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!fb7u!f47=n_mq9n7yg-3_7y!pev2wzw09!at3=1a@h&cwgy_1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', const=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
