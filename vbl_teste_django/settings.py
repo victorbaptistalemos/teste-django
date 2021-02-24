0daae90f271330489591df03a37b12f2b15e57c7
Victor Baptista Lemos <victorbaptistalemos@icloud.com>
2021-02-24 16:29:18 +0000

    SECRET_KEY não está mais exposta

--- vbl_teste_django/settings.py
+++ vbl_teste_django/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!fb7u!f47=n_mq9n7yg-3_7y!pev2wzw09!at3=1a@h&cwgy_1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
